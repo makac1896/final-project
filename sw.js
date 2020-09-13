f5b815e5d5db9f5727535516e8af59f76ae513d6
zenithsoftwares2019 <71141521+zenithsoftwares2019@users.noreply.github.com>
2020-09-13 08:24:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,25 @@
-let cacheName = 'site-static';
+const cacheName = 'site-static';
+const cacheAssets = [
+                    'index.html',
+                    'fallback.html',
+                    'css/style.css',
+                    'manifest.json',
+                    'app.js',
+                    'img/bg1.jpg',
+                    'img/bg2.jpg',
+                    'img/icon1.svg',
+                    'calendar.png',
+                    'coffee-icon.png'
+]
 
 
 self.addEventListener('install', evt => {
     console.log("Service Worker Installed")
     evt.waitUntil(
-        caches.open(cacheName).then(cache =>{
-            cache.addAll(
-                [
-                    '/',
-                    '/index.html',
-                    '/fallback.html',
-                    '/css/style.css',
-                    '/manifest.json',
-                    '/app.js',
-                    '/img/bg1.jpg',
-                    '/img/bg2.jpg',
-                    '/img/icon1.svg',
-                    '/calendar.png',
-                    '/coffee-icon.png'
-                ]
-            )
-        })
+        caches.open(cacheName)
+        .then(cache =>{
+            cache.addAll(cacheAssets)
+        }).then(()=> self.skipWaiting())
     )
 })
 
